402d74942f42731eba98eed6af23be4ff9ecaa74
Mohd Faraz <mohd.faraz.abc@gmail.com>
2020-01-06 14:42:50 +0530

    panda01a: [HAXX] for fixing Wlan in prebuilt kernel
    Change-Id: I2467b40ac14f29b1c9a3c2512f6a6d5ac836991d
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -171,6 +171,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# module
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/modules/pronto_wlan.ko:system/lib/modules/wlan.ko
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
