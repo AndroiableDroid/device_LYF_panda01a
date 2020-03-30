e614809146f5edb9643fe590815483dd1f630bb9
Mohd Faraz <androiabledroid@gmail.com>
2020-03-30 12:22:58 +0530

    panda01a: update init
     * increase heap value to 4m
    
    Change-Id: I453fdf32ec1189f3e96434bb83d071177d7aceac
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- init/init_panda01a.cpp
+++ init/init_panda01a.cpp
@@ -25,32 +25,88 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/sysinfo.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <private/android_filesystem_config.h>
 
+#include "vendor_init.h"
 #include "property_service.h"
+#include "log.h"
+#include "util.h"
 
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
+#include <sys/resource.h>
 
-void check_device()
+int read_file2(const char *fname, char *data, int max_size)
 {
-        // from - phone-xxhdpi-2048-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heapminfree = "2m";
+    int fd, rc;
+
+    if (max_size < 1)
+        return 0;
+
+    fd = open(fname, O_RDONLY);
+    if (fd < 0) {
+        ERROR("failed to open '%s'\n", fname);
+        return 0;
+    }
+
+    rc = read(fd, data, max_size - 1);
+    if ((rc > 0) && (rc < max_size))
+        data[rc] = '\0';
+    else
+        data[0] = '\0';
+    close(fd);
+
+    return 1;
+}
+
+void init_alarm_boot_properties()
+{
+    char *alarm_file = "/proc/sys/kernel/boot_reason";
+    char buf[64];
+    char tmp[PROP_VALUE_MAX]="";
+
+    property_get("ro.boot.alarmboot", tmp);
+
+    if(read_file2(alarm_file, buf, sizeof(buf))) {
+
+    /*
+     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+     * For existing PMIC chips, the following mapping applies
+     * for the value of boot_reason:
+     *
+     * 0 -> unknown
+     * 1 -> hard reset
+     * 2 -> sudden momentary power loss (SMPL)
+     * 3 -> real time clock (RTC)
+     * 4 -> DC charger inserted
+     * 5 -> USB charger insertd
+     * 6 -> PON1 pin toggled (for secondary PMICs)
+     * 7 -> CBLPWR_N pin toggled (for external power supply)
+     * 8 -> KPDPWR_N pin toggled (power key pressed)
+     */
+        if(buf[0] == '3' || !strcmp(tmp,"true"))
+            property_set("ro.alarm_boot", "true");
+        else
+            property_set("ro.alarm_boot", "false");
+    }
 }
 
 void vendor_load_properties()
 {
-    check_device();
 
-    property_set("dalvik.vm.heapstartsize", heapstartsize);
-    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", heapsize);
+    property_set("dalvik.vm.heapstartsize", "16m");
+    property_set("dalvik.vm.heapgrowthlimit", "192m");
+    property_set("dalvik.vm.heapsize", "512m");
     property_set("dalvik.vm.heaptargetutilization", "0.75");
-    property_set("dalvik.vm.heapminfree", heapminfree);
+    property_set("dalvik.vm.heapminfree", "4m");
     property_set("dalvik.vm.heapmaxfree", "8m");
+    property_set("media.msm8956hw", "1");
+    property_set("media.settings.xml", "/etc/media_profiles_8956.xml");
+    property_set("media.msm8956.version", "1");
+
+    init_alarm_boot_properties();
 }
